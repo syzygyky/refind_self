82a56bd4dfa5c998bec4e8de71211d1e3d946013
syzygyky <156642805+syzygyky@users.noreply.github.com>
2024-03-13 04:56:21 +0900

    Update lang.js

--- lang.js
+++ lang.js
@@ -3,7 +3,7 @@ function switchLang(){
 
   const category = document.getElementById('category');
   const categoryText = {
-    ja:['--全て表示--', '最初のエリア', 'ベンチ', 'バッテリー、占い', '神社', 'こころ・羊の絵', '鏡', 'きぼう', 'その他の物体', 'いのり', '羊の世話', 'リサイクルショップ', '森のNPC', '自動販売機', '猫', '玩具型M2', '望遠鏡', 'だる・カフェ', 'シューティングゲーム', '研究所', 'アクションゲーム', 'たばこ', 'みらい', 'カジノ', '洞窟', '玩具型M1', '海岸', '防衛戦', '倒れている人', 'いろいろ', '最後の行動', 'エンディング'],
+    ja:['--全て表示--', '最初のエリア', 'ベンチ', 'バッテリー、占い', '神社', 'こころ・羊の絵', '鏡', 'きぼう', 'その他の物体', 'いのり', '羊の世話', 'リサイクルショップ', '森のNPC', '自動販売機', '猫', '玩具型M2', '望遠鏡', 'だる・カフェ', 'シューティングゲーム', '研究所', 'アクションゲーム', 'たばこ', 'みらい・ハイアンドロー', 'カジノ', '洞窟', '玩具型M1', '海岸', '防衛戦', '倒れている人', 'いろいろ', '最後の行動', 'エンディング'],
     en:['--Show All--', 'First Area', 'Bench', 'Battery, Divigation', 'Shrine', 'Kokoro/Sheep painting', 'Mirror', 'Kibou', 'Other object', 'Inori', 'Caring sheep', 'Recycle Shop', 'Forest NPC', 'Vending machine', 'Cat', 'Toy Type M2', 'Telescope', 'Dull/Café', 'Shooting game', 'Labo', 'Action game', 'Cigarette', 'Mirai/High-Low', 'Casino', 'Cave', 'Toy Type M1', 'Coast', 'Defense', 'Collapsed man', 'Miscellaneous', 'Final action', 'Ending']
   }
   for(let i=0; i<category.options.length; i++){
